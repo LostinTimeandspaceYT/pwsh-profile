c08cc12139851124098ffe1873ce9968fa8f60f0
Nate <48383244+LostinTimeandspaceYT@users.noreply.github.com>
2025-01-23 13:34:01 -0500

    Added lsusb and placeholder for serial shell import

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/space.omp.json" | Invoke-Expression
 
 function fuzzy_find {
     fzf --preview 'bat --color=always --line-range :500 {}'
@@ -27,6 +27,9 @@ function ls_tree {
 function ls_a {
   lsd -la
 }
+function get_com_ports {
+  Get-CimInstance -Class Win32_SerialPort | Select-Object Name, Description, DeviceID
+}
 
 # Personal Aliases
 Set-Alias fzy fuzzy_find
@@ -34,3 +37,7 @@ Set-Alias du disk_usage
 Set-Alias lt ls_tree 
 Set-Alias la ls_a 
 Set-Alias -Name ls -Value lsd -Option AllScope
+Set-Alias -Name lsusb -Value get_com_ports -Option AllScope
+
+# Load Custom Serial Shell
+# Import-Module (TODO! Add fpath here)
